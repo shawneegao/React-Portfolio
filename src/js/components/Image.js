40b9325c0a3756f396f3cf359e010d69b2c3a7e1
shawneegao <shawneegao@gmail.com>
2017-01-20 18:12:54 -0600

    built

--- src/js/components/Image.js
+++ src/js/components/Image.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { Component } from 'react';
+import Lightbox from 'react-image-lightbox';
 var Masonry = require('react-masonry-component');
 
 var masonryOptions = {
@@ -8,35 +9,45 @@ var masonryOptions = {
 };
 
 
-var Image = React.createClass({
+export default class Portfolio extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+        isOpen: false
+    };
+  }
+  render(){
+    console.log(this.props)
 
-    render:function(){
-      console.log(this.props)
-
-      var imageWidth = this.props.style
-      var childElements = this.props.elements.imageInfo.map(function(element){
-          return (
-               <div class= "image-wrapper" >
+    var imageWidth = this.props.style
+    var isOpen = false;
+    var openLightbox = function(){
+      isOpen = true
+      console.log(isOpen)
+    }
+    console.log(isOpen)
+    var childElements = this.props.elements.imageInfo.map(function(element){
+        return (
+             <div class= "image-wrapper" >
                 <div class= "text-wrapper">
                   <span class = "piece-title fred"> {element.title} </span>
                   <p class ="jose">{element.caption}</p>
                 </div>
-                   <img className="piece" style ={imageWidth} src={element.src}/>
-               </div>
-           );
-       });
-       return (
-            <Masonry
-                className={'my-gallery-class'} // default ''
-                elementType={'ul'} // default 'div'
-                options={masonryOptions} // default {}
-                disableImagesLoaded={false} // default false
-                updateOnEachImageLoad={true} // default false and works only if disableImagesLoaded is false
-            >
-            {childElements}
-            </Masonry>
-        );
-    }
-});
-
-module.exports = Image;
+                   <img onClick= {openLightbox} className="piece" style ={imageWidth} src={element.src}/>
+                   {isOpen && <div>SOMETHING IS THERE </div>}
+             </div>
+         );
+     });
+     return (
+          <Masonry
+              className={'my-gallery-class'} // default ''
+              elementType={'ul'} // default 'div'
+              options={masonryOptions} // default {}
+              disableImagesLoaded={false} // default false
+              updateOnEachImageLoad={true} // default false and works only if disableImagesLoaded is false
+          >
+          {childElements}
+          </Masonry>
+      );
+  }
+};
